9e239fbb5a1e9af8d6136b87e617beb7a99ee402
SurrealTheCoder <57976268+SurrealTheCoder@users.noreply.github.com>
2020-11-20 09:09:03 -0500

    Add files via upload

--- db-log.php
+++ db-log.php
@@ -2,18 +2,16 @@
 header("Location: http://macrohard.epizy.com/Confirmation.html");
 exit();
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-$conn = mysqli_connect('localhost', 'username', 'paassword') or die (mysqli_error());
-mysqli_select_db('db_name') or die (mysqli_error());
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_error());
+$dbName = $_SERVER["DOCUMENT_ROOT"] . "products\products.mdb";
+if (!file_exists($dbName)) {
+    die("Could not find database file.");
 }
+$db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb)}; DBQ=$dbName; Uid=; Pwd=;");
 
-$sqli = "INSERT INTO local (email, firstname, lastname, address, city, state, ZIP, Phone) VALUES ('$_POST [email]', '$_POST [firstname]', '$_POST [lastname]', '$_POST [address]', '$_POST [city]', '$_POST [state]', '$_POST [ZIP]', '$_POST [Phone]')";
+$sqli = "INSERT INTO product (FirstName, LastName, Prefix, Email, ShippingAddress1, ShippingAddress2, City, State, ZIP, BillingAddress1, BillingAddress2, City2, State2, ZIP2, PhoneNumber) 
+VALUES ('$_POST[FirstName]' '$_POST[LastName]' '$_POST[Prefix]' '$_POST[Email]' '$_POST[ShippingAddress1]' '$_POST[ShippingAddress2]' '$_POST[City]' '$_POST[State]' '$_POST[ZIP]' '$_POST[BillingAddress1]' '$_POST[BillingAddress2]' '$_POST[City2]' '$_POST[State2]' '$_POST[ZIP2]' '$_POST[PhoneNumber]'";
+
+$db->query($sqli);
 
 mysqli_close($conn);
    
