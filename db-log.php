64d79812a341ad2a91b83fc4107b3654ba225fa2
SurrealTheCoder <ryan42901@outlook.com>
2021-01-20 14:59:23 -0500

    Add files via upload

--- db-log.php
+++ db-log.php
@@ -2,17 +2,68 @@
 header("Location: http://macrohard.epizy.com/Confirmation.html");
 exit();
 
-$dbName = $_SERVER["DOCUMENT_ROOT"] . "products\products.mdb";
-if (!file_exists($dbName)) {
-    die("Could not find database file.");
-}
-$db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb)}; DBQ=$dbName; Uid=; Pwd=;");
+    try {
+        $conn = new PDO("odbc:formdatabase", "", "");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch (PDOException $e) {
+        echo "Conection failed: " . $e->getMessage();
+    }
 
-$sqli = "INSERT INTO product (FirstName, LastName, Prefix, Email, ShippingAddress1, ShippingAddress2, City, State, ZIP, BillingAddress1, BillingAddress2, City2, State2, ZIP2, PhoneNumber) 
-VALUES ('$_POST[FirstName]' '$_POST[LastName]' '$_POST[Prefix]' '$_POST[Email]' '$_POST[ShippingAddress1]' '$_POST[ShippingAddress2]' '$_POST[City]' '$_POST[State]' '$_POST[ZIP]' '$_POST[BillingAddress1]' '$_POST[BillingAddress2]' '$_POST[City2]' '$_POST[State2]' '$_POST[ZIP2]' '$_POST[PhoneNumber]'";
+$sql = "INSERT INTO `checkout` (prefix, firstName, lastName, email, ccn, cvv, expiry, streetAddress1, streetAddress2, city, state1, zip, billingAddress1, billingAddress2, city2, state2, zip2, phoneNumber)
+                                        VALUES (:prefix, :firstName, :lastName, :email, :ccn, :cvv, :expiry, :streetAddress1, :streetAddress2, :city, :state1, :zip, :billingAddress1, :billingaddress2, :city2, :state2, :zip2, :phoneNumber)";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(":prefix", $prefix);
+$stmt->bindParam(":firstName", $firstName);
+$stmt->bindParam(":lastName", $lastName);
+$stmt->bindParam(":email", $email);
+$stmt->bindParam(":ccn", $ccn);
+$stmt->bindParam(":cvv", $cvv);
+$stmt->bindParam(":expiry", $expiry);
+$stmt->bindParam(":streetAddress1", $streetAddress1);
+$stmt->bindParam(":streetAddress2", $streetAddress2);
+$stmt->bindParam(":city", $city);
+$stmt->bindParam(":state1", $state1);
+$stmt->bindParam(":zip", $zip);
+$stmt->bindParam(":billingAddress1", $billingAddress1);
+$stmt->bindParam(":billingAddress2", $billingAddress2);
+$stmt->bindParam(":city2", $city2);
+$stmt->bindParam(":state2", $state2);
+$stmt->bindParam(":zip2", $zip2);
+$stmt->bindParam(":phoneNumber", $phoneNumber);
 
-$db->query($sqli);
 
-mysqli_close($conn);
-   
+$form = $_POST;
+$prefix = $form[ 'prefix' ];                    
+$firstName = $form[ 'firstName' ];                   
+$lastName = $form[ 'lastName' ];                 
+$email = $form[ 'email' ];   
+$ccn = $form[ 'ccn' ];
+$cvv = $form[ 'cvv' ];
+$expiry = $form[ 'expiry' ];
+$streetAddress1 = $form[ 'streetAddress1' ];
+$streetAddress2 = $form[ 'streetAddress2' ];
+$city = $form[ 'city' ];
+$state1 = $form[ 'state1' ];
+$zip = $form[ 'zip' ];
+$billingAddress1 = $form[ 'billingAddress1' ];
+$billingAddress2 = $form[ 'billingAddress2' ];
+$city2 = $form[ 'city2' ];
+$state2 = $form[ 'state2' ];
+$zip2 = $form[ 'zip2' ];
+$phoneNumber = $form[ 'phoneNumber' ];              
+
+
+$result = $stmt->execute();
+
+if($result == null) {
+    echo '<script>alert("Your Transaction could not be processed, please try again soon.")</script>';
+
+    }// end if
+else {
+    echo '<script>alert("Thank You for Checking Out!")</script>';
+    }// end else
+
+    exit();
+
 ?>
